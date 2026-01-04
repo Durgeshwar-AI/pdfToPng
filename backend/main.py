0fb75bfb4385ffd35ec348043a06ed4ceaeb8885
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2026-01-04 15:05:50 +0530

    Fixed the port error

--- backend/main.py
+++ backend/main.py
@@ -145,4 +145,4 @@ def remove_background():
 # ---------------- RUN ---------------- #
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
